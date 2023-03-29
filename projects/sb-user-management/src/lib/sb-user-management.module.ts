fbb213326e936a2e0e8e78e6615790ee2674fb11
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-03-29 17:33:06 +0530

    chore : table view for user list

--- projects/sb-user-management/src/lib/sb-user-management.module.ts
+++ projects/sb-user-management/src/lib/sb-user-management.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { SbUserManagementComponent } from './sb-user-management.component';
 import { ButtonModule } from 'primeng/button';
 import { UserListComponent } from './user-list/user-list.component';
 import { TabViewModule } from 'primeng/tabview';
-
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [SbUserManagementComponent, UserListComponent],
   imports: [
+    BrowserModule,
+    CommonModule,
     ButtonModule,
-    TabViewModule
+    TabViewModule,
+    TableModule,
+    InputTextModule
   ],
   exports: [SbUserManagementComponent]
 })
